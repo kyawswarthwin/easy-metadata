2917567198b6f8090935fe865ea9851ef5efee3e
Kyaw Swar Thwin <myanmarunicorn@gmail.com>
2018-09-08 06:44:00 +0630

    add jimp

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 'use strict';
 
 const mm = require('music-metadata');
+const jimp = require('jimp');
 
 function metadata(filePath) {
   return new Promise(async (resolve, reject) => {
     try {
       const { native, common } = await mm.parseFile(filePath, { native: true });
+      let picture = common.picture && (await jimp.read(common.picture[0].data));
+      picture =
+        picture &&
+        (await picture
+          .resize(300, jimp.AUTO)
+          .quality(60)
+          .getBase64Async(common.picture[0].format));
       const metadata = {
         title: common.title,
         artist: common.artists && getUniqueValue(common.artists[0]),
         album: common.album,
-        albumArtist: common.albumartist,
         year: common.year && common.year.toString(),
         track: common.track.no,
         genre: common.genre && getUniqueValue(common.genre[0]),
-        cover:
-          common.picture &&
-          `data:${common.picture[0].format};base64,${common.picture[0].data.toString('base64')}`
+        picture: picture
       };
       if (native['iTunes MP4']) {
         const iTunes = mm.orderTags(native['iTunes MP4']);
